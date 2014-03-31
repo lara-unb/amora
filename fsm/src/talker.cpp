2c0802ae6f23155fdc276ef0f52972f73a7d81ae
Gastd <gabriel.araujo.5000@gmail.com>
2014-03-31 17:43:04 -0300

    New Talker node

--- fsm/src/talker.cpp
+++ fsm/src/talker.cpp
@@ -1,65 +1,31 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "festival.h"
-#include "fsm.h"
 
-typedef struct Buffer
+void Utterance(const std_msgs::String::ConstPtr& msg)
 {
-	std_msgs::String string;
-	ros::Time timestamp;
-}msg_Buffer;
-
-msg_Buffer buffer;
-int state = 0;
-bool state_wait = false;
-
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-	//cout << msg->data.c_str() << endl;
-	buffer.string = *msg;
-	buffer.timestamp = ros::Time::now();
-	//state = 2;
-	fsm::check(msg->data.c_str(), &state, &state_wait);
+	festival_say_text(EST_String(msg->data.c_str()));
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+	ros::init(argc, argv, "talker");
+	
+	ros::NodeHandle n;
+	
+	ros::Subscriber sub = n.subscribe("mouth", 1000, Utterance);
+	
+	int heap_size = FESTIVAL_HEAP_SIZE;  // default scheme heap size
+	int load_init_files = 1; // we want the festival init files loaded
 
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-
-   while (ros::ok())
-   {
-   	switch(state)
-   	{
-   		case 0:
-   			fsm::init();
-   			state++;
-   			break;
-   		case 1:
-   			fsm::wait();
-   			break;
-   		case 2:
-   			fsm::ack(buffer.timestamp, &state, &state_wait);
-   			break;
-   		case 3:
-   			fsm::follow();
-   			state = 1;
-   			state_wait = false;
-   			break;
-   		case 4:
-   			fsm::tour_guide();
-   			state = 1;
-   			state_wait = false;
-   			break;
-   	}
-   	
-   	ros::spinOnce();
-   }
-
-
-  return 0;
+	festival_initialize(load_init_files,heap_size);
+	
+	festival_say_text(EST_String("HI. I AM ARAMIS"));
+	
+	while(ros::ok())
+	{
+		ros::spinOnce();
+	}
+	
+	return 0;
 }
-
