54d82d1209fa59bdc99f500ed56dfb07a5874ba4
Gastd <gabriel.araujo.5000@gmail.com>
2014-08-29 21:44:26 -0300

    New module for new system (ubuntu 14)

--- fsm/cmake/FindSpeechtools.cmake
+++ fsm/cmake/FindSpeechtools.cmake
@@ -18,9 +18,9 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-find_path(SPEECHTOOLS_INCLUDE_DIRS EST/EST.h
+find_path(SPEECHTOOLS_INCLUDE_DIRS speech_tools/EST.h
 		  HINTS /usr/include /usr/local/include
-		  PATH_SUFFIXES EST)
+		  PATH_SUFFIXES speech_tools)
 
 find_library(ESTBASE_LIBRARIES estbase
 		   HINTS /usr/lib/ usr/local/lib)
@@ -30,7 +30,7 @@ find_library(ESTSTRING_LIBRARIES eststring
 		   HINTS /usr/lib/ usr/local/lib)
 
 SET(SPEECHTOOLS_LIBRARIES ${ESTOOLS_LIBRARIES} ${ESTBASE_LIBRARIES} ${ESTSTRING_LIBRARIES})
-SET(SPEECHTOOLS_INCLUDE_DIR ${SPEECHTOOLS_INCLUDE_DIRS}/EST)
+SET(SPEECHTOOLS_INCLUDE_DIR ${SPEECHTOOLS_INCLUDE_DIRS}/speech_tools)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Festival DEFAULT_MSG
